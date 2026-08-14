1ff67cff32af5280837c2680385de73b0fe273b4
Juan Leonidas Espeche <juan.l.espeche@gmail.com>
2026-08-14 20:38:43 -0300

    fix(test): inline firebase deps in vitest.config.ts so vi.mock is consistently applied in CI

--- vertex-platform/vitest.config.ts
+++ vertex-platform/vitest.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    deps: {
+      optimizer: {
+        web: {
+          enabled: false,
+        },
+        ssr: {
+          enabled: false,
+        },
+      },
+      inline: [
+        '@firebase/auth',
+        'firebase/auth',
+        '@firebase/functions',
+        'firebase/functions',
+        '@firebase/app',
+        'firebase/app',
+      ],
+    },
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
